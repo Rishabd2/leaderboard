892aed85b6ac681f866efc47e5dd2b827e96daee
rishabd2 <rishabd2@illinois.edu>
2023-07-11 09:15:10 +0530

    Update Leaderboard.js

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,53 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { fetchAttendeesPoints } from '../api';
 import './Leaderboard.css';
+import * as THREE from 'three';
+import { FontLoader } from 'src\FontLoader.js';
+
+
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
-  
+
   useEffect(() => {
     const limit = 10;
 
+    const fetchAttendeesPoints = async (limit) => {
+      try {
+        const response = await fetch(`API_URL?limit=${limit}`);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    };
+
     fetchAttendeesPoints(limit)
-      .then(data => {
+      .then((data) => {
         if (data !== null) {
           setLeaderboardData(data);
         } else {
-          console.log('Failed to fetch leaderboard data');
+          console.log("Failed to fetch leaderboard data");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-
-    const cardControllers = document.querySelectorAll("[data-card-controller]");
-
-    cardControllers.forEach(controller => {
-      controller.addEventListener("click", (e) => {
-        const card = e.currentTarget.parentElement.parentElement;
-        const isVisible = card.dataset.visible;
-
-        if (isVisible === "false") {
-          card.setAttribute("data-visible", true);
-        } else {
-          card.setAttribute("data-visible", false);
-        }
-      });
-    });
-
-    // Clean up event listeners when the component is unmounted
-    return () => {
-      cardControllers.forEach(controller => {
-        controller.removeEventListener("click", () => {});
-      });
-    };
   }, []);
-  
 
   const scoreBoard = leaderboardData.map((player, index) => ({
     name: player.discord,
     viewCount: `${player.points} points`,
   }));
+
   const buttonClick = (value) => {
     // Handle button click logic here
     console.log(`Button clicked: ${value}`);
@@ -56,6 +49,7 @@ const Leaderboard = () => {
   return (
     <div className="container">
       <div className="frame">
+      
       <header className="heading">My Leaderboard</header> {/* Add a heading element */}
         <header>Leaderboard</header>
         <div className="button-group" data-left="4px">
@@ -67,7 +61,8 @@ const Leaderboard = () => {
           {scoreBoard.map((item, index) => (
             <div className="leader" key={index}>
               <div className="user">
-                <div className="number">{index}</div>
+                <div className="number">{index+1}</div>
+                
                 <div className="user-pic"></div>
               </div>
               <div className="user-info">
